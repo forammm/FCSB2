fa2c84a58c5f04ef70968b89ca1915404667b3fb
Foram_Mehta <91962703+forammm@users.noreply.github.com>
2024-08-04 12:10:37 +0930

    Update question02.sh

--- question02.sh
+++ question02.sh
@@ -2,7 +2,7 @@
 git init
 git add .
 git commit -m "Question2"
-git remote add origin https://github.com/forammm/assignment_git.git
+git remote add origin https://github.com/forammm/FCSB2.git
 git push -u origin main
-git remote add origin https://github.com/forammm/assignment_git.git
+git remote add origin https://github.com/forammm/FCSB2.git
 git push -u origin main
