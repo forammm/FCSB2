e66dab052e94c73ebeec53ed366f84b5ca81b9ae
Foram_Mehta <91962703+forammm@users.noreply.github.com>
2024-08-04 12:09:16 +0930

    Add files via upload

---
+++ question02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git init
+git add .
+git commit -m "Question2"
+git remote add origin https://github.com/forammm/assignment_git.git
+git push -u origin main
+git remote add origin https://github.com/forammm/assignment_git.git
+git push -u origin main
